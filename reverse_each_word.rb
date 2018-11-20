d830cc7f9e04da051d1ec18abacf2b1ac0c9885f
Marie Burns <mcburns2222@gmail.com>
2018-11-20 01:55:13 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,7 @@
 def reverse_each_word(string)
-  reverse_words = []
   array = string.split
-  array.each do |word|
+  array.collect do |word|
     reverse_words << word.reverse!
   end
-  reverse_words.join(" ")
 end
   
-  def reverse_each_word()
